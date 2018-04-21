2ff1efe1887090671891797e10b4eff920f6eaaf
asbonato <asbonato@192.168.0.15>
2018-04-20 21:37:21 -0300

    Versão inicial.

---
+++ arqsw_teste/src/entity/Aluno.java
@@ -0,0 +1,43 @@
+package entity;
+
+public class Aluno {
+	private int ra;
+	private String nome;
+	
+	public int getRa() {
+		return ra;
+	}
+	public void setRa(int ra) {
+		this.ra = ra;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (ra != other.ra)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Aluno [ra=" + ra + ", nome=" + nome + "]";
+	}
+	
+}
